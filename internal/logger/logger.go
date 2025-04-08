a33ad9d3cbfc318588433e58e69bf7fd78bbd97b
meopedevts <marcelo.meope@gmail.com>
2025-04-08 15:36:49 -0300

    ♻️ refactor: Remove comentários e simplifica Logger
    Remove comentários redundantes e documentação interna obsoleta.
    Simplifica a estrutura Logger eliminando campo não utilizado.
    Refatora método logMessage removendo código comentado.
    Melhora legibilidade e manutenção do código de logging.

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -8,7 +8,6 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
-// LogLevel representa os níveis de log
 type LogLevel int
 
 const (
@@ -19,24 +18,15 @@ const (
 	FATAL
 )
 
-// Logger é a estrutura principal para o logger customizado
-type Logger struct {
-	// Level LogLevel
-}
+type Logger struct{}
 
-// Instância global do logger
 var Log *Logger
 
-// InitLogger inicializa o logger
 func InitLogger() {
 	Log = &Logger{}
 }
 
 func (l *Logger) logMessage(level LogLevel, msg string, args ...any) {
-	// if level < l.Level {
-	// 	return
-	// }
-
 	var style lipgloss.Style
 	var prefix string
 
@@ -62,28 +52,23 @@ func (l *Logger) logMessage(level LogLevel, msg string, args ...any) {
 	log.Printf("%s %s\n", style.Render(prefix), message)
 }
 
-// Debug imprime uma mensagem de debug
 func Debug(msg string, args ...any) {
 	Log.logMessage(DEBUG, msg, args...)
 }
 
-// Info imprime uma mensagem informativa
 func Info(msg string, args ...any) {
 	Log.logMessage(INFO, msg, args...)
 }
 
-// Warn imprime uma mensagem de aviso
 func Warn(msg string, args ...any) {
 	Log.logMessage(WARN, msg, args...)
 }
 
-// Error imprime uma mensagem de erro
 func Error(msg string, args ...any) {
 	Log.logMessage(ERROR, msg, args...)
 }
 
-// Fatal imprime uma mensagem de erro fatal
 func Fatal(msg string, args ...any) {
 	Log.logMessage(FATAL, msg, args...)
-	os.Exit(1) // Encerra o programa após logar a mensagem fatal
+	os.Exit(1)
 }
